77ba592190ce424a01c5e27ac3171a1f82487286
Pin Wang <pinwang@gmail.com>
2025-08-11 12:58:23 +0800

    Bump version to 0.1.3

--- py-build/build-windows.ps1
+++ py-build/build-windows.ps1
@@ -2,7 +2,7 @@
 param(
     [string]$ProjectRoot = (Split-Path -Parent $PSScriptRoot),
     [string]$AppName = "whisper-key",
-    [string]$AppVersion = "0.1.2",
+    [string]$AppVersion = "0.1.3",
     [switch]$Clean
 )
 
